220c0fb6bb12e9c2a761f4185e06fb17f8e15352
noah <noahgray@me.com>
2014-12-23 08:34:25 -0800

    adds categories based on visible offers

--- app.js
+++ app.js
@@ -18,27 +18,44 @@ var app = angular.module('store', ['ngRoute'])
 
 .controller('CategoryController', [ '$http', '$routeParams', '$route', '$location', function($http, $routeParams, $route, $location){
 	var store = this;
-	this.message = "hello world";
 	this.$route = $route;
 	this.$location = $location;
 	this.$routeParams = $routeParams;
+	this.manufacturer = $routeParams.manufacturer;
 	this.offer = [];
+	this.categories = [];
+	this.apiArgs = 'manufacturer=' + this.manufacturer;
+	console.log(this.$routeParams.category);
+	
+	if('category' in this.$routeParams) {
+		this.category = $routeParams.category;
+		this.apiArgs = this.apiArgs + '&categoryPath.name=' + this.category;
+	}
 
-	$http.get('http://api.remix.bestbuy.com/v1/products(manufacturer=' + store.$routeParams.manufacturer + ')?format=json&show=sku,name,salePrice&apiKey=y7ne8hezdwv8dxa3j8ncgxfn').success(function(data) {
+	$http.get('http://api.remix.bestbuy.com/v1/products(' + store.apiArgs + ')?format=json&show=categoryPath.name,categoryPath.id,sku,name,salePrice&apiKey=y7ne8hezdwv8dxa3j8ncgxfn')
+	.success(function(data) {
 			store.offers = data.products;
-	}).error( function (data, status) {
-		console.log(status);
-		console.log(store.$routeParams);
-	});
-
-	$http.get('http://api.remix.bestbuy.com/v1/categories(name=' + store.$routeParams.manufacturer +')?format=json&apiKey=y7ne8hezdwv8dxa3j8ncgxfn').success(function(data) {
-			store.categories = data.categories[0].subCategories;
+			store.offers.forEach(function(each){
+				if (objectArrayContains(store.categories, each.categoryPath[1].id, 'id')) {
+					store.categories.push({
+						'name': each.categoryPath[1].name,
+						'id': each.categoryPath[1].id
+					});
+				}
+			});
 			console.log(store.categories);
 	}).error( function (data, status) {
 		console.log(status);
-		console.log(store.$routeParams);
+		console.log(store.apiArgs);
 	});
 
 }]);
+
+function objectArrayContains(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) return false;
+    }
+    return -1;
+}
 
 })(window.angular);
