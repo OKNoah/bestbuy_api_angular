b5cbf5450b1850bb7888fbdfd0220787f487d91e
noah <noahgray@me.com>
2014-12-23 10:27:31 -0800

    adds working category filter working with brand filter.

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ var app = angular.module('store', ['ngRoute'])
 	$routeProvider
 		.when('/maker/:manufacturer', {
 			controller: 'CategoryController',
-			templateUrl: 'store.html'
+			templateUrl: 'category.html'
 		})
 		.when('/maker/:manufacturer/category/:category', {
 			controller: 'CategoryController',
-			templateUrl: 'store.html'
+			templateUrl: 'category.html'
 		});
 }])
 
 .controller('CategoryController', [ '$http', '$routeParams', '$route', '$location', function($http, $routeParams, $route, $location){
 	var store = this;
-	this.$route = $route;
-	this.$location = $location;
 	this.$routeParams = $routeParams;
 	this.manufacturer = $routeParams.manufacturer;
 	this.offer = [];
@@ -27,23 +25,26 @@ var app = angular.module('store', ['ngRoute'])
 	this.apiArgs = 'manufacturer=' + this.manufacturer;
 	console.log(this.$routeParams.category);
 	
+	//
+	// BUILD THE SEARCH ARGUMENTS, IF /CATEGORY IS IN URL
+	//
 	if('category' in this.$routeParams) {
 		this.category = $routeParams.category;
-		this.apiArgs = this.apiArgs + '&categoryPath.name=' + this.category;
+		this.apiArgs = this.apiArgs + '&categoryPath.id=' + this.category;
 	}
 
-	$http.get('http://api.remix.bestbuy.com/v1/products(' + store.apiArgs + ')?format=json&show=categoryPath.name,categoryPath.id,sku,name,salePrice&apiKey=y7ne8hezdwv8dxa3j8ncgxfn')
+	$http.get('http://api.remix.bestbuy.com/v1/products(' + store.apiArgs + ')?format=json&show=categoryPath.name,categoryPath.id,sku,name,salePrice,thumbnailImage&apiKey=y7ne8hezdwv8dxa3j8ncgxfn')
 	.success(function(data) {
 			store.offers = data.products;
 			store.offers.forEach(function(each){
-				if (objectArrayContains(store.categories, each.categoryPath[1].id, 'id')) {
+				if (objectUniqueToArray(store.categories, each.categoryPath[1].id, 'id')) {
 					store.categories.push({
 						'name': each.categoryPath[1].name,
 						'id': each.categoryPath[1].id
 					});
 				}
 			});
-			console.log(store.categories);
+			console.log(data);
 	}).error( function (data, status) {
 		console.log(status);
 		console.log(store.apiArgs);
@@ -51,7 +52,10 @@ var app = angular.module('store', ['ngRoute'])
 
 }]);
 
-function objectArrayContains(myArray, searchTerm, property) {
+//
+// FUNCTION TO CHECK IF OBJECT ALREADY EXISTS IN ARRAY OF OBJECTS
+//
+function objectUniqueToArray(myArray, searchTerm, property) {
     for(var i = 0, len = myArray.length; i < len; i++) {
         if (myArray[i][property] === searchTerm) return false;
     }
